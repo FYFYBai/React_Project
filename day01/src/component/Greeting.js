15f6b98756b69407b25ec083c285362d7ec09f00
FYFYBai <93832388+FYFYBai@users.noreply.github.com>
2025-06-03 03:04:13 -0400

    adding components

--- day01/src/component/Greeting.js
+++ day01/src/component/Greeting.js
@@ -3,6 +3,7 @@ function Greeting() {
   //JSX example
   const name = "Qi He";
   const message = <h1>Hello, {name}!</h1>;
+  const isStudent = true; //boolean
   //we use {} to pass a dynamic value to JSX.
   return (
     <div>
@@ -10,7 +11,9 @@ function Greeting() {
         Hello, World! FSD13-React-01 Qi.
         <br />
         This is a message using JSX: {message}
+        <h2>Student : {isStudent ? "Yes" : "No"}</h2> 
       </header>
+      {/* ?: is a conditional rendering, meaning if true, print yes, if false, print no. */}
     </div>
   );
 }
